e9fc91ca378ad2dedaa14be27e463470c9288f43
Georgi <georgi.ginduzov17d@gmail.com>
2024-01-30 23:48:58 +0200

    Old mid exam excercise

---
+++ 01. C# Fundamentals May.2023/FundamentalsMidExams/SoftuniReception/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SoftuniReception
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentsPerEmployeePerHour = 0;
+            
+            for (int i = 0; i < 3; i++)
+            {
+                studentsPerEmployeePerHour += int.Parse(Console.ReadLine());
+            }
+
+            double studentsCount = int.Parse(Console.ReadLine());
+            /*
+            double time = Math.Ceiling(studentsCount / studentsPerEmployeePerHour);
+            double restTime = Math.Ceiling(time / 4);
+
+            if (time < 4)
+            {
+                restTime -= 1;
+            }*/
+
+
+
+            int time = 0;
+            while (studentsCount > 0)
+            {
+                studentsCount -= studentsPerEmployeePerHour;
+                time++;
+                if (time % 4 == 0)
+                {
+                    time++;
+                }
+            }
+
+             Console.WriteLine($"Time needed: {time}h.");
+        }
+    }
+}
