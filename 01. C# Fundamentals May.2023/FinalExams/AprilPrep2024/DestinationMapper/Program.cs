27dbfdf2113ebaa6c5ebf50e41991a7dcf1a9530
Georgi <georgi.ginduzov17d@gmail.com>
2024-04-06 18:18:03 +0300

    Implement regex filtering

---
+++ 01. C# Fundamentals May.2023/FinalExams/AprilPrep2024/DestinationMapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([=/])([A-Z][a-zA-Z]{2,})\1";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            List<string> refactoredMatches = new List<string>();
+            
+            int travelPoints = 0;
+
+            foreach (Match match in matches)
+            {
+                string tempMatch = Regex.Replace(match.ToString(), "([=/])", "");
+                refactoredMatches.Add(tempMatch);
+                travelPoints += tempMatch.Length;
+            }
+
+            
+            Console.WriteLine("Destinations: " + String.Join(", ", refactoredMatches));
+            Console.WriteLine("Travel Points: " + travelPoints);
+        }
+    }
+}
