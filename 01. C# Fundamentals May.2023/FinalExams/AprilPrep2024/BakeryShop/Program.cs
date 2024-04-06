d258d24ae8207bc355e78ebcf7fc2ffc87d2fd12
Georgi <georgi.ginduzov17d@gmail.com>
2024-04-06 18:14:27 +0300

    Implement shop class

---
+++ 01. C# Fundamentals May.2023/FinalExams/AprilPrep2024/BakeryShop/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace BakeryShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop bakery = new Shop();
+            string[] input = Console.ReadLine().Split(' ');
+
+            while (input[0] != "Complete")
+            {
+                int quantity = int.Parse(input[1]);
+                string item = input[2];
+
+                switch (input[0])
+                {
+                    case "Receive":
+                        bakery.receiveItem(quantity, item);
+                        break;
+                    case "Sell":
+                        bakery.sellItem(quantity, item);
+                        break;
+                }
+
+                input = Console.ReadLine().Split(' ');
+            }
+
+            bakery.printStockAvailability();
+            Console.WriteLine($"All sold: {bakery.Soldstock} goods");
+        }
+    }
+}
---
+++ 01. C# Fundamentals May.2023/FinalExams/AprilPrep2024/BakeryShop/Shop.cs
@@ -0,0 +1,95 @@
+﻿
+namespace BakeryShop
+{
+    public class Shop
+    {
+        private Dictionary<string, int> stock;
+        private int soldstock;
+
+        public Shop()
+        {
+            this.Stock = new Dictionary<string, int>();
+            this.Soldstock = 0;
+        }
+
+        public Dictionary<string, int> Stock { get; set; }
+        public int Soldstock
+        {
+            get
+            {
+                return this.soldstock;
+            }
+            set
+            {
+                this.soldstock = value;
+            }
+        }
+
+        public bool receiveItem(int quantity, string name)
+        {
+            if (quantity <= 0)
+            {
+                return false;
+            }
+
+            addItem(quantity, name);
+
+            return true;
+        }
+
+        private void addItem(int quantity, string name)
+        {
+            if (Stock.ContainsKey(name))
+            {
+                Stock[name] += quantity;
+            }
+            else
+            {
+                Stock[name] = quantity;
+            }
+        }
+
+        public bool sellItem(int quantity, string item)
+        {
+            if (Stock.ContainsKey(item))
+            {
+                if (Stock[item] - quantity >= 0)
+                {
+
+                    Soldstock += quantity;
+
+                    Stock[item] -= quantity;
+                    Console.WriteLine($"You sold {quantity} {item}.");
+                    if (Stock[item] == 0)
+                    {
+                        Stock.Remove(item);
+                    }
+                }
+                else
+                {
+                    int soldQuantity = Stock[item];
+                    Stock[item] = 0;
+                    Stock.Remove(item);
+
+                    Soldstock += soldQuantity;
+
+                    Console.WriteLine($"There aren't enough {item}. You sold the last {soldQuantity} of them.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"You do not have any {item}.");
+            }
+
+            return true;
+        }
+
+        public void printStockAvailability()
+        {
+            foreach (var item in Stock)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
