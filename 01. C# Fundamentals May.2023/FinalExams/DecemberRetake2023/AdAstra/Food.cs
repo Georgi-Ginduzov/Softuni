4b8b210d6e497220c66844200025283abd739d0e
Georgi <georgi.ginduzov17d@gmail.com>
2024-04-07 14:00:48 +0300

    Implement days left to live given an input of foods

---
+++ 01. C# Fundamentals May.2023/FinalExams/DecemberRetake2023/AdAstra/Food.cs
@@ -0,0 +1,16 @@
+﻿namespace AdAstra
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int Calories { get; set; }
+
+        public Food(string name, DateTime expirationDate, int calories)
+        {
+            Name = name;
+            ExpirationDate = expirationDate;
+            Calories = calories;
+        }
+    }
+}
---
+++ 01. C# Fundamentals May.2023/FinalExams/DecemberRetake2023/AdAstra/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace AdAstra
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Food> foodList = new List<Food>();
+            int minimumCaloriesPerDay = 2000;
+
+            string input = Console.ReadLine();
+            string pattern = @"([#|])(?<name>[A-Za-z\s]+)\1(?<expirationDate>\d{2}\/\d{2}\/\d{2})\1(?<calories>\d+)\1";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                string name = match.Groups["name"].Value;
+                DateTime expirationDate = DateTime.ParseExact(match.Groups["expirationDate"].Value, "dd/MM/yy", CultureInfo.InvariantCulture);
+                int calories = int.Parse(match.Groups["calories"].Value);
+
+                foodList.Add(new Food(name, expirationDate, calories));
+            }
+
+            int daysToLastWithFood = foodList.Sum(food => food.Calories) / minimumCaloriesPerDay;
+
+            Console.WriteLine($"You have food to last you for: {daysToLastWithFood} days!");
+            foreach (var food in foodList)
+            {
+                Console.WriteLine($"Item: {food.Name}, Best before: {food.ExpirationDate.ToString("dd/MM/y")}, Nutrition: {food.Calories}");
+            }
+        }
+    }
+}
