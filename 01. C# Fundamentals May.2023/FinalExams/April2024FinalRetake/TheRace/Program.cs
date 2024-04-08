5765b5c3f87d37ffc445745bdd1d59b353e7d992
Georgi <georgi.ginduzov17d@gmail.com>
2024-04-08 12:31:07 +0300

    Final Refactorings

---
+++ 01. C# Fundamentals May.2023/FinalExams/April2024FinalRetake/TheRace/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TheRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string pattern = @"([#$%*&])(?<name>[A-Za-z]+)\1=(?<length>\d+)!!(?<code>.+)";
+            bool foundCoordinates = false;
+
+            while (foundCoordinates == false)
+            {
+                input = Console.ReadLine();
+
+                Match match = Regex.Match(input, pattern);
+                
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+                    int length = int.Parse(match.Groups["length"].Value);
+                    string code = match.Groups["code"].Value;
+
+                    if (length == code.Length)
+                    {
+                        StringBuilder sb = new StringBuilder();
+
+                        for (int i = 0; i < code.Length; i++)
+                        {
+                            sb.Append((char)(code[i] + length));
+                        }
+
+                        Console.WriteLine($"Coordinates found! {name} -> {sb}");
+                        foundCoordinates = true;
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ 01. C# Fundamentals May.2023/FinalExams/April2024FinalRetake/WariorQuest/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+
+namespace WariorQuest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder skill = new StringBuilder(Console.ReadLine());
+            
+            string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder commandBuild = new StringBuilder();
+            commandBuild.AppendJoin(' ', command);
+
+            while (commandBuild.ToString().Trim() != "For Azeroth")
+            {
+                switch (command[0])
+                {
+                    case "GladiatorStance":
+                        for (int i = 0; i < skill.Length; i++)
+                        {
+                            skill[i] = Char.ToUpper(skill[i]);
+                        }
+                        Console.WriteLine(skill);
+                        break;
+                    case "DefensiveStance":
+                        for (int i = 0; i < skill.Length; i++)
+                        {
+                            skill[i] = Char.ToLower(skill[i]);
+                        }
+                        Console.WriteLine(skill);
+                        break;
+                    case "Dispel":
+                        int index = int.Parse(command[1]);
+                        char letter = char.Parse(command[2]);
+
+                        if (index >= 0 && index < skill.Length)
+                        {
+                            skill = skill.Remove(index, 1).Insert(index, letter.ToString());
+                            Console.WriteLine("Success!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dispel too weak.");
+                        }
+                        break;
+                    case "Target":
+                        if (command[1] == "Change")
+                        {
+                            string substring = command[2];
+                            string secondSubstring = command[3];
+
+                            skill = skill.Replace(substring, secondSubstring);
+
+                            Console.WriteLine(skill);
+                        }
+                        else if (command[1] == "Remove")
+                        {
+                            string substring = command[2];
+
+                            if (skill.ToString().Contains(substring))
+                            {
+                                skill = skill.Replace(substring, "");
+                                Console.WriteLine(skill);
+
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Command doesn't exist!");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Command doesn't exist!");
+                        break;
+                }
+
+                command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                commandBuild = new StringBuilder();
+                commandBuild.AppendJoin(' ', command);
+            }
+        }
+    }
+}
