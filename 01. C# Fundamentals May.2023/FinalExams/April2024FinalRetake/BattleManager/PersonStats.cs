87a8b30800096a80e1da7c37ed02120dbed19866
Georgi <georgi.ginduzov17d@gmail.com>
2024-04-08 09:34:52 +0300

    Implement combat logic

---
+++ 01. C# Fundamentals May.2023/FinalExams/April2024FinalRetake/BattleManager/PersonStats.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleManager
+{
+    public class PersonStats
+    {
+        public int Health { get; set; }
+        public int Energy { get; set; }
+
+        public PersonStats(int health, int energy)
+        {
+            Health = health;
+            Energy = energy;
+        }
+    }
+}
---
+++ 01. C# Fundamentals May.2023/FinalExams/April2024FinalRetake/BattleManager/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+namespace BattleManager
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split(':');
+
+            Dictionary<string, PersonStats> people = new Dictionary<string, PersonStats>();
+
+            while (command[0] != "Results")
+            {
+                string commandType = command[0];
+
+                switch (commandType)
+                {
+                    case "Add":
+                        string personName = command[1];
+                        int health = int.Parse(command[2]);
+                        int energy = int.Parse(command[3]);
+
+                        if (people.ContainsKey(personName))
+                        {
+                            people[personName].Health += health;
+                        }
+                        else
+                        {
+                            people[personName] = new PersonStats(health, energy);
+                        }
+                        break;
+                    case "Attack":
+                        string attackerName = command[1];
+                        string defenderName = command[2];
+                        int damage = int.Parse(command[3]);
+
+                        if (people.ContainsKey(attackerName) && people.ContainsKey(defenderName))
+                        {
+                            people[defenderName].Health -= damage;
+                            people[attackerName].Energy--;
+
+                            if (people[defenderName].Health <= 0)
+                            {
+                                people.Remove(defenderName);
+                                Console.WriteLine($"{defenderName} was disqualified!");
+                            }
+                            if (people[attackerName].Energy <= 0)
+                            {
+                                people.Remove(attackerName);
+                                Console.WriteLine($"{attackerName} was disqualified!");
+                            }
+                        }
+
+                        break;
+                    case "Delete":
+                        string username = command[1];
+
+                        if (username == "All")
+                        {
+                            people.Clear();
+                            command = Console.ReadLine().Split(':');
+                            continue;
+                        }
+
+                        if (people.ContainsKey(username))
+                        {
+                            people.Remove(username);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+
+                command = Console.ReadLine().Split(':');
+            }
+
+            StringBuilder output = new StringBuilder();
+            output.AppendLine($"People count: {people.Count}");
+            foreach (var person in people)
+            {
+                output.AppendLine($"{person.Key} - {person.Value.Health} - {person.Value.Energy}");
+            }
+
+            Console.WriteLine(output.ToString().Trim());
+        }
+    }
+}
