89f1b41b2c39c9509fe74ff1b0953ddf68a85d79
Georgi <georgi.ginduzov17d@gmail.com>
2024-04-13 22:39:41 +0300

    Input exceptions

---
+++ 03. C# OOP June.2023/ExceptionsErrorHandling/EnterNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace EnterNumbers
+{
+    internal class Program
+    {
+        static void ReadNumber(int start, int end)
+        {
+            List<int> validNumbers = new List<int>();
+            int number = 1;
+
+            while (validNumbers.Count < 10)
+            {
+                try
+                {
+                    number = int.Parse(Console.ReadLine().Trim());
+                    
+                    if (number <= start || number >= end)
+                    {
+                        throw new ArithmeticException();
+                    }
+
+                    validNumbers.Add(number);
+                    start = number;
+                }
+                catch (ArithmeticException)
+                {
+                    Console.WriteLine($"Your number is not in range {start} - 100!");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+            }
+            Console.WriteLine(String.Join(", ", validNumbers));
+        }
+        static void Main(string[] args)
+        {
+            ReadNumber(1, 100);
+        }
+    }
+}
