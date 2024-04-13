6c10b230dd627d5ee8e58c898ecd86f4edf7d0f9
Georgi <georgi.ginduzov17d@gmail.com>
2024-04-13 22:40:30 +0300

    Input validation of exceptions

---
+++ 03. C# OOP June.2023/ExceptionsErrorHandling/SquareRoot/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            try
+            {
+                if (number < 0) {
+                    throw new ArithmeticException();
+                }
+
+                number = Math.Sqrt(number);
+                Console.WriteLine(number);
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
